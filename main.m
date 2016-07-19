0ca672010dcbd39634f2f03948d4f8e98079e4cd
simchanu29 <simon.chanu@ensta-bretagne.org>
2016-07-19 13:18:15 +0300

    only bearing update

--- main.m
+++ main.m
@@ -1,9 +1,9 @@
-path(path,'C:\matlab_sailing\Matlab-files\2 Egen regulator');
-path(path,'C:\matlab_sailing\new');
-path(path,'C:\matlab_sailing\GPS_POS');
-path(path,'C:\matlab_sailing\collision_avoidance/Tools');
-path(path,'C:\matlab_sailing\collision_avoidance/Sensors_and_OnboardTools');
-path(path,'C:\matlab_sailing\collision_avoidance/Control');
+path(path,'C:\matlab_sailing\GitRepo\Matlab_boat_simulation\Matlab-files\2 Egen regulator');
+path(path,'C:\matlab_sailing\GitRepo\Matlab_boat_simulation\new');
+path(path,'C:\matlab_sailing\GitRepo\Matlab_boat_simulation\GPS_POS');
+path(path,'C:\matlab_sailing\GitRepo\Matlab_boat_simulation\collision_avoidance/Tools');
+path(path,'C:\matlab_sailing\GitRepo\Matlab_boat_simulation\collision_avoidance/Sensors_and_OnboardTools');
+path(path,'C:\matlab_sailing\GitRepo\Matlab_boat_simulation\collision_avoidance/Control');
 theta = wrapToPi(pi/2-theta/180*pi);
 delay = 2; %smith delay in seconds 
 size_buffer_delay = 500;
@@ -62,9 +62,80 @@ while time_t >= time_delay(1) + delay && i_delay>1
     time_delay = time_delay_temp;
 end
 
-if obstacle_mode
+%% command code
+
+if obstacle_mode==1
     %% obstacle avoidance
-    % 
+    % [delta_r, delta_s, q, tacking] = controller_simpleLine_v_control(m, theta,v,q, psi_tw,a,b,tacking);
+    % delta_r = delta_r/2;
+
+    % Loopback values : tacking, avoidMode, is_obstacle_detected, NGZmode. 
+    % Initialized values : 
+        % x
+        x = [m(1),m(2),theta,v,0] % w is not used in the script
+        nWayP = k;
+        % Initialization
+        if(~exist('posObstacles','var'))
+            posObstacles = [1.19   191;
+                           -11.16 -284;
+                           -1.47  -281;
+                           -28.6  -726;
+                            46.6   21.1]; % utm coordinates - start
+            phat = posWaypoint(:,nWayP);
+            qhat = [];
+            distDetect = 10; 
+            angleDetect = pi/4;
+            followedLine = [x(1:2) phat];
+            rq = 10;
+            avoidCollisionPoint = x(1:2);
+            % takes the furthest waypoints and use it as a box with a safety margin
+            xmin = min(gps_waypoint(:,1))-50;
+            xmax = max(gps_waypoint(:,1))+50;
+            ymin = min(gps_waypoint(:,2))-50;
+            ymax = max(gps_waypoint(:,2))+50;
+            dim = [xmin xmax ymin ymax];
+            X = linspace(xmin,xmax,100);
+            Y = linspace(ymin,ymax,100);
+            [P1,P2] = meshgrid(X,Y);
+            
+            % vObs = [0 1 0 -1 0
+            %         0 1 0 -1 0] ;
+            vObs = [];
+            if(isempty(vObs)==0)
+                movingObstacles = find(vObs(1,:)~=0 & vObs(2,:)~=0);
+            end
+            
+            is_obstacle_detected = 0;
+            NGZmode = 0;
+            avoidMode = 0;
+        end    
+    % Obstacle detection
+    detectedObstacles = obstacle_detection(x,posObstacles',distDetect,angleDetect);  
+    qhat = update_obstacles(qhat,detectedObstacles,x,distDetect,angleDetect); % cleaning added
+    % The sensors check if a obstacle is detected, then add its coordinates
+    % to the obstacle database (=qhat)
+    [collisionnedObstacle,avoidMode,is_obstacle_detected] = boat_on_collision_course(x,qhat,rq,r,avoidMode,is_obstacle_detected);
+
+    % Path following (Waypoint system)
+    [nWayP,phat,followedLine,avoidMode] = next_waypoint(x,phat,followedLine,avoidMode,collisionnedObstacle,gps_waypoint',nWayP,r);
+
+    % Avoid script
+    [avoidCollisionPoint,followedLine,avoidMode] = avoid_obstacle(avoidMode, followedLine, collisionnedObstacle,avoidCollisionPoint,qhat,phat,x,P1,P2,psi_tw,rq,r,dim); % using global var
+    
+    % Simulation
+    if(isempty(vObs)==0)
+        posObstacles = moveObstacle(posObstacles',movingObstacles,vObs,dt);
+        posObstacles = posObstacles';
+    end
+    
+    % Command
+    [u,q,NGZmode]=follow_line(x,q,psi_tw,followedLine,NGZmode);
+
+    % interface with old code
+    delta_r = u(1);
+    delta_s = u(2);
+elseif(obstacle_mode==2)
+    %% obstacle avoidance
     % [delta_r, delta_s, q, tacking] = controller_simpleLine_v_control(m, theta,v,q, psi_tw,a,b,tacking);
     % delta_r = delta_r/2;
 
